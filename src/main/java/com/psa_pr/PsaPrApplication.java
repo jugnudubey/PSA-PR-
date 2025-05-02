f4e2852927fed6716cda9daa21ec79ab444e7569
Jugnu Dubey <jug.jugnu08@gmail.com>
2025-05-02 18:36:37 +0530

    First commit

---
+++ src/main/java/com/psa_pr/PsaPrApplication.java
@@ -0,0 +1,13 @@
+package com.psa_pr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsaPrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsaPrApplication.class, args);
+	}
+
+}
